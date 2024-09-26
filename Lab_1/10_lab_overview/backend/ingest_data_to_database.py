c7b71bba3b46a8605161fa42e0b0a7905f4645be
Sara Mellgren <143341964+saraonsala@users.noreply.github.com>
2024-09-26 12:12:18 +0200

    Made som comments

--- Lab_1/10_lab_overview/backend/ingest_data_to_database.py
+++ Lab_1/10_lab_overview/backend/ingest_data_to_database.py
@@ -1,16 +1,18 @@
 from database import Database
 from constants import CLEANED_DATA_PATH, DATABASE_PATH
 
+#   Function to ingest the cleaned csv data to duckdb
+
 def ingest_csv_data_to_duckdb():
     """ingesting the cleaned csv data to duckdb"""
-    translation_table = str.maketrans({"å": "a", "ä": "a", "ö": "o"})
+    translation_table = str.maketrans({"å": "a", "ä": "a", "ö": "o"}) # Translation table for swedish characters
 
-    for directory_path in CLEANED_DATA_PATH.glob("*"):
+    for directory_path in CLEANED_DATA_PATH.glob("*"): # Loop through all the directories in the cleaned data path
         schema_name = directory_path.name.lower().translate(translation_table)
-        for csv_path in directory_path.glob("*"):
-            table_name = csv_path.name.lower().split(".")[0].translate(translation_table)
+        for csv_path in directory_path.glob("*"): 
+            table_name = csv_path.name.lower().split(".")[0].translate(translation_table) # Table name is the csv file name without the extension
 
-            with Database(DATABASE_PATH) as db:
+            with Database(DATABASE_PATH) as db: # Connect to the database
                 db.query(f"CREATE SCHEMA IF NOT EXISTS {schema_name};")
                 db.query(
                     f"""
