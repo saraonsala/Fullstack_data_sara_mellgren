0d2358214374e57afd232c8360c3d71f6039177b
Sara Mellgren <143341964+saraonsala@users.noreply.github.com>
2024-10-09 19:20:22 +0200

    marts

--- Lab_1/backend/sql/Scripts/marts_gender_tred.sql
+++ Lab_1/backend/sql/Scripts/marts_gender_tred.sql
@@ -1,12 +1,81 @@
-CREATE TABLE IF NOT EXISTS marts.gender_trend AS 
+CREATE TABLE IF NOT EXISTS marts.gender_trend_clean_data AS (
+WITH 
+	Tittare.tabelldata_kon AS (SELECT * FROM tittare.tabelldata_kon."Tittarnas alder"),
+	tittare.tabelldata_alder ta AS (SELECT * FROM  tittare.tabelldata_alder."Tittarnas kon")
+SELECT 
+	tittare.tabelldata_alder."Tittarnas kon" ,
+	tittare.tabelldata_kon."Tittarnas alder" 
+	tittare.tabelldata_kon."Genomsnittlig visningslängd" 
+FROM tittare.tabelldata_alder ta as "Kön"
+LEFT JOIN tittare.tabelldata_kon e as "ålder" 
+ON tittare.tabelldata_alder = tittare.tabelldata_kon;
+
+
+
+
+
+
+---CREATE SCHEMA IF NOT EXISTS marts;
+
+CREATE TABLE IF NOT EXISTS marts.videos_title AS 
+(
 SELECT
-    tabelldata_kon."Tittarnas alder" AS "Tittarnas ålder",
-    tabelldata_alder."Tittarnas kon" AS "Tittarnas kön",
+	STRFTIME('%Y-%m-%d',
+	Datum) AS Datum ,
+	Videotitel,
+	Visningar
 FROM
-    tittare.tabelldata_alder
+	innehall.diagramdata);
+
+
+SELECT * FROM marts.videos_title;
+CREATE TABLE IF NOT EXISTS marts.gender_trend_clean_data AS (
+SELECT
+	tittare.tabelldata_kon "Tittarnas alder" AS "Tittarnas ålder",
+	tittare.tabelldata_alder."Tittarnas kon" AS "Tittarnas kön",
+FROM
+	tittare.tabelldata_alder
 INNER JOIN
     tittare.tabelldata_kon
 ON
-    tittare.tabelldata_alder."Tittarnas kon" = tittare.tabelldata_kon."Tittarnas alder" 
+	tittare.tabelldata_alder."Tittarnas kon" = tittare.tabelldata_kon."Tittarnas alder";
+
+	sum(Visningar) AS Visningar
+FROM
+	enhetstyp.diagramdata d
+GROUP BY
+	(datum,
+	Enhetstyp )
+ORDER BY 
+	"Tittarens_kön" );
+
+CREATE TABLE IF NOT EXISTS marts.device_summary AS (
+SELECT
+	Enhetstyp,
+	Visningar,
+	"Visningstid (timmar)" AS Visningstid_timmar,
+	"Genomsnittlig visningslängd" AS Visningslängd_genomsnitt
+FROM
+	enhetstyp.tabelldata );
 
 
+CREATE TABLE IF NOT EXISTS marts.gender_trend AS (
+SELECT
+	tittare.tabelldata_kon "Tittarnas alder" AS "Tittarnas ålder",
+	tittare.tabelldata_alder."Tittarnas kon" AS "Tittarnas kön",
+FROM
+	tittare.tabelldata_alder
+INNER JOIN
+    tittare.tabelldata_kon
+ON
+	tittare.tabelldata_alder."Tittarnas kon" = tittare.tabelldata_kon."Tittarnas alder";
+
+
+
+
+
+
+
+-- checks
+SELECT * FROM marts.device_views_date;
+SELECT * FROM marts.device_summary;
--- Lab_1/frontend/graphs.py
+++ Lab_1/frontend/graphs.py
@@ -15,21 +15,34 @@ class GenderTrend:
 
         # Gruppera datan för att summera antalet tittare per kön
         gender_counts = self.df.groupby("Tittarnas kön").size().reset_index(name="Antal Tittare")
-
+        print(self.df)
+        # fig = px.bar(gender_counts, )
         # Skapa ett cirkeldiagram för könsfördelning
         fig = px.pie(gender_counts, names="Tittarnas kön", values="Antal Tittare", 
                      title="Könsfördelning av Tittare")
 
         # Lägg till interaktivitet och stilinställningar
         fig.update_traces(textinfo="percent+label", hoverinfo="label+percent+value")
-        fig.update_layout(autosize=True, plot_bgcolor='rgba(0,0,0,0)')
+        # fig.update_layout(autosize=True, plot_bgcolor='rgba(0,0,0,0)')
         
         # Visa grafen
         st.plotly_chart(fig)
 
-# Skapa en instans av klassen och visa diagrammet
-#gender_trend = GenderTrend()
-#gender_trend.display_plot()
+# ViewsTrend klassen
+    # class ViewsTrend:
+    #     def __init__(self) -> None:
+    #         self.df = QueryDatabase("SELECT * FROM marts.views_trend").df
+
+    #     def display_plot(self):
+    #         st.markdown("## Visningstrend")
+    #         # Skapa en linjediagram för visningstrend
+    #         fig, ax = plt.subplots()
+    #         ax.plot(self.df["Datum"], self.df["Visningar"], marker="o", color="r")
+    #         ax.set_title("Visningstrend")
+    #         ax.set_xlabel("Datum")
+    #         ax.set_ylabel("Visningar")
+    #         ax.grid(True)
+    #         st.pyplot(fig)
 
 
 
