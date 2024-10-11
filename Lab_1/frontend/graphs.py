b1ee1b84a6da970c1b60754b402c2f1ddf50a982
Sara Mellgren <143341964+saraonsala@users.noreply.github.com>
2024-10-11 11:18:52 +0200

    Ändrat subheder

--- Lab_1/frontend/graphs.py
+++ Lab_1/frontend/graphs.py
@@ -30,7 +30,7 @@ class DeviceDate:
 
     def display_plot(self):
         # Visa nya visualiseringar
-        st.subheader("Device Views Over Time")
+        st.subheader("Enhetsvyer över tid")
         device_views_date_df = self.df
         fig_views_date = px.line(device_views_date_df, x='Datum', y='Visningar', color='Enhetstyp', title='Visningar per Enhetstyp över Tid')
         fig_views_date.update_layout(autosize=True, plot_bgcolor='rgba(0,0,0,0)')
@@ -42,18 +42,18 @@ class DeviceSummary:
         self.df = QueryDatabase("SELECT * FROM marts.device_summary;").df
         
     def display_plot(self):
-        st.subheader("Device Summary")
+        st.subheader("Sammanfattning av enhetstyper")
         device_summary_df= self.df
         fig_summary = px.bar(device_summary_df, x='Enhetstyp', y='Visningar', title='Sammanfattning av Visningar per Enhetstyp')
         fig_summary.update_layout(autosize=True, plot_bgcolor='rgba(0,0,0,0)')
         st.plotly_chart(fig_summary, use_container_width=True)
 
-        st.subheader("Average Watch Time per Device")
+        st.subheader("Genomsnittlig visningstid per enhet")
         fig_avg_watch_time = px.bar(device_summary_df, x='Enhetstyp', y='Visningslängd_genomsnitt', title='Genomsnittlig Visningslängd per Enhetstyp')
         fig_avg_watch_time.update_layout(autosize=True, plot_bgcolor='rgba(0,0,0,0)')
         st.plotly_chart(fig_avg_watch_time, use_container_width=True)
 
-        st.subheader("Total Watch Time per Device")
+        st.subheader("Total visningstid per enhet")
         fig_total_watch_time = px.bar(device_summary_df, x='Enhetstyp', y='Visningstid_timmar', title='Total Visningstid (timmar) per Enhetstyp')
         fig_total_watch_time.update_layout(autosize=True, plot_bgcolor='rgba(0,0,0,0)')
         st.plotly_chart(fig_total_watch_time, use_container_width=True)
